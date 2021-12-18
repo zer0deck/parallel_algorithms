0fe658302fc630699116cd79d0dccc6760145459
zer0deck <zer0deck@icloud.com>
2021-12-18 21:38:56 +0300

    lab 1 fix

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -7,58 +7,55 @@
 
 int main(int argc, char** argv)
 {
-    if (argc > 1) // if we pass arguments to function, argc will be more than 1
+if (argc > 1) // blocks the programm if there are no entered arguments
+{
+    int vec_size = atoi(argv [1]); // size of the vector
+    std::vector<double> randArray(vec_size);
+
+    for (auto &vec_item: randArray) // Fill in random variables
     {
-        int vec_size = atoi(argv [1]); // size of the vector
+        vec_item = (double)rand() / (RAND_MAX/10);
+    }
 
-        std::vector<double> randArray(vec_size);
-
-        for (auto &vec_item: randArray) // Fill in random variables
+    if (argc > 2 and vec_size <= 10) // we test the programm for 10 arguments (as written in an exercise)
+    {
+        std::cout << "Elements of the array: ";
+        for (auto &vec_item: randArray)
         {
-            vec_item = (double)rand() / (RAND_MAX/10);
+            std::cout << vec_item << "  ";
         }
-        // Print elements if pass 2nd arg and our array is not very big
+        std::cout << std::endl;
+    }
+
+    for(auto thread_num = 1; thread_num < 11; thread_num++)
+    {
+        double maximum_value = -1.0; 
+        auto time_start = std::chrono::high_resolution_clock::now(); // c.time
+
+#pragma omp parallel for num_threads(thread_num) reduction (max:maximum_value)
+
+        for (auto i = 0; i < vec_size; i++)
+        {
+            if (randArray[i] > maximum_value)
+            {
+                maximum_value = randArray[i];
+            }
+        }
+
+        auto time_stop = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time_stop - time_start);
+        std::cout << "Thread: " << thread_num << " ET(ms): " <<  duration.count() << " ";
+
+        //Enter any thind argument to see elements (if array is less than eleven)
         if (argc > 2 and vec_size <= 10)
         {
-            std::cout << "Elements of the array: ";
-            for (auto &vec_item: randArray)
-            {
-                std::cout << vec_item << "  ";
-            }
-            std::cout << std::endl;
-        }
-        // iterate over the number of threads
-        for(auto thread_num = 1; thread_num < 11; thread_num++)
-        {
-            double max_val = -1.0; // as all the elements in array between 0 to 1
-
-            auto time_start = std::chrono::high_resolution_clock::now(); // get the current time
-
-#pragma omp parallel for num_threads(thread_num) reduction (max:max_val)
-            for (auto i = 0; i < vec_size; i++)
-            {
-                if (randArray[i] > max_val)
-                {
-                    max_val = randArray[i];
-                }
-            }
-            auto time_stop = std::chrono::high_resolution_clock::now(); // get the stop time
-
-            auto duration = std::chrono::duration_cast<std::chrono::microseconds>(time_stop - time_start); // calculate the duration
-
-            std::cout << "Number of threads:" << thread_num << " Execution time (in microseconds): " <<  duration.count() << std::endl;
-
-            // Print elements if pass 2nd arg and our array is not very big
-            if (argc > 2 and vec_size <= 10)
-            {
-                std::cout << "Found maxval: " << max_val << std::endl;
-
-            }
+            std::cout << "Maximum value: " << maximum_value << std::endl;
         }
     }
-    else
-    {
-        std::cout << "Not arguments were given" << std::endl;
-    }
-    return 0;
+}
+else
+{
+std::cout << "You hadn't entered any arguments" << std::endl;
+}
+return 0;
 }
