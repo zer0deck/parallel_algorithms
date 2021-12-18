375ddd624586d67e0e655393a0cf32de758938ae
zer0deck <zer0deck@icloud.com>
2021-12-18 20:45:44 +0300

    lab 1 cooms

--- Lab_1.cpp
+++ Lab_1.cpp
@@ -12,13 +12,13 @@ int main(int argc, char** argv){
         double* randArray = new double[vec_size];
         for (long int i = 0; i < vec_size; i++) { //Generate number between 0 to 1
             randArray[i] = (double)rand() / RAND_MAX;
-        } 
+        }
         //cout<<"\nElements of the array::"<<endl;
         // printing elements of an array
         //for(int i = 0; i < vec_size; i++)
         //    cout<<"Elements no "<< i + 1<< "::" << randArray[i] <<endl;
         //  }
-        
+
         // iterate over the number of threads
         for(int thread_num = 1; thread_num < 11; thread_num++){
             double max_val = -1.0; // as all the elements in array between 0 to 1
