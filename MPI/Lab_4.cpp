cdaa5d6524ca1df400dfde4069dd27daf54c59fd
zer0deck <zer0deck@icloud.com>
2021-12-19 16:59:17 +0300

    lab 4 final

--- MPI/Lab_4.cpp
+++ MPI/Lab_4.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <stdio.h>
 #include "mpi.h"
+#include <iostream>
 using namespace std;
 
 int main(int argc, char* argv[]) {
@@ -9,17 +10,18 @@ int main(int argc, char* argv[]) {
 	MPI_Status status;
 	MPI_Comm_size(MPI_COMM_WORLD, &n);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	
-	if (rank == 0)
+
+	if (rank == 7)
 	{
 		cout << "Hello from process " << rank << "\n";
-		for (i  = 1; i < n; i++) {
+		for (i = 1; i < n; i++) {
 			MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-			cout << "Hello from process " << message << endl;
+			cout << "Hello from process " << double(message) / 7 << endl;
+
 		}
 	}
 
-	else MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+	else MPI_Send(&rank, 1, MPI_INT, 7, 0, MPI_COMM_WORLD);
 	MPI_Finalize();
 
 	return 0;
