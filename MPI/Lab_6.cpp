1144be7b8e94dc8cc2738a8536e9762bc590a7f2
zer0deck <zer0deck@icloud.com>
2021-12-25 21:21:22 +0300

    lab 6 final

--- MPI/Lab_6.cpp
+++ MPI/Lab_6.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 #include <mpi.h>
+
 using namespace std;
+
 int main(int argc, char **argv)
 {
 	int rank, size, ibuf;
 	MPI_Status status;
 	float rbuf;
-	// Initialize MPI
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	ibuf = rank;
 	rbuf = 1.0 * rank;
-	if (rank == 1) MPI_Send(&ibuf, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
-	if (rank == 2) MPI_Send(&rbuf, 1, MPI_FLOAT, 0, 2, MPI_COMM_WORLD);
-	if (rank == 0) {
-		MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-		if (status.MPI_TAG == 1) {
-			MPI_Recv(&ibuf, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
-			MPI_Recv(&rbuf, 1, MPI_FLOAT, 2, 2, MPI_COMM_WORLD, &status);
-			cout << "Process 0 recv " << ibuf << " from process 1, " << rbuf << " from process 2\n";
+	if (rank == 1) MPI_Send(&ibuf, 1, MPI_INT, 0, 5, MPI_COMM_WORLD);
+	if (rank == 2) MPI_Send(&rbuf, 1, MPI_FLOAT, 0, 6, MPI_COMM_WORLD);
+	if (rank == 0)
+    {
+		MPI_Probe(MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status); // Check who is the first one
+		if (status.MPI_TAG == 5)
+        {
+			MPI_Recv(&ibuf, 1, MPI_INT, MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status);
+			MPI_Recv(&rbuf, 1, MPI_FLOAT, MPI_ANY_SOURCE, 6, MPI_COMM_WORLD, &status);
+			cout << "Process 0 recv " << ibuf << " from process 1, " << rbuf << " from process 2"<< endl;
 		}
-		else if (status.MPI_TAG == 2) {
-			MPI_Recv(&rbuf, 1, MPI_FLOAT, 2, 2, MPI_COMM_WORLD, &status);
-			MPI_Recv(&ibuf, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, &status);
-			cout << "Process 0 recv " << rbuf << " from process 2, " << ibuf << " from process 1\n";
+		else if (status.MPI_TAG == 6)
+        {
+			MPI_Recv(&rbuf, 1, MPI_FLOAT, MPI_ANY_SOURCE, 6, MPI_COMM_WORLD, &status);
+			MPI_Recv(&ibuf, 1, MPI_INT, MPI_ANY_SOURCE, 5, MPI_COMM_WORLD, &status);
+			cout << "Process 0 recv " << rbuf << " from process 2, " << ibuf << " from process 1" << endl;
 		}
 	}
 	MPI_Finalize();
